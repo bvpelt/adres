f2bc037bc7c0d243d78b030a6d1bba6e182d6370
bvpelt <bart.vanpelt@gmail.com>
2024-11-28 14:03:55 +0100

    WIP

--- src/main/java/com/bsoft/adres/config/WebSecurityConfig.java
+++ src/main/java/com/bsoft/adres/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
@@ -23,9 +22,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
-@Profile("runtime")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -58,12 +57,13 @@ public class WebSecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-                    config.setAllowedMethods(Arrays.asList("*")); // Allow all HTTP methods
-                    config.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
+                    config.setAllowedOrigins(List.of("http://localhost:4200"));
+                    config.setAllowedMethods(List.of("*")); // Allow all HTTP methods
+                    config.setAllowedHeaders(List.of("*")); // Allow all headers
                     return config;
                 }))
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .securityMatcher("/**")
@@ -71,6 +71,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/login/**", "/error").permitAll()
                         .requestMatchers(HttpMethod.GET, "/adres/api/v1/adresses/**", "/persons/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/adres/api/v1/adresses/**", "/persons/**").hasAnyAuthority("READ", "WRITE")
