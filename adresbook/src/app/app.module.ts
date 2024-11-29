0bb5a8b4b24b577809dd4f784fc32f363836033c
bvpelt <bart.vanpelt@gmail.com>
2024-11-29 13:58:53 +0100

    Frontend

--- adresbook/src/app/app.module.ts
+++ adresbook/src/app/app.module.ts
@@ -5,10 +5,9 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiModule } from './core/modules/openapi';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { HttpClientModule } from '@angular/common/http';
 
-import { CachingInterceptor } from './interceptors/caching.interceptor';
 import { BASE_PATH } from './core/modules/openapi';
 import { AdresesComponent } from './adreses/adreses.component';
 import { AdresComponent } from './adres/adres.component';
@@ -29,8 +28,7 @@ import { AdresdetailComponent } from './adresdetail/adresdetail.component';
     HttpClientModule
   ],
   providers: [
-//    { provide: BASE_PATH, useValue: "http:/localhost:8080/adres/api/v1" } 
-    {provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true }
+//    { provide: BASE_PATH, useValue: "http:/localhost:8080/adres/api/v1" }     
   ],
   bootstrap: [AppComponent]
 })
