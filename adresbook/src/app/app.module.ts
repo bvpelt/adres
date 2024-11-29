8a5e1b9c69d3b67a5e48487097f3899abb6b3082
bvpelt <bart.vanpelt@gmail.com>
2024-11-29 13:22:40 +0100

    WIP

--- adresbook/src/app/app.module.ts
+++ adresbook/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiModule } from './core/modules/openapi';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+
+import { CachingInterceptor } from './interceptors/caching.interceptor';
 import { BASE_PATH } from './core/modules/openapi';
 import { AdresesComponent } from './adreses/adreses.component';
 
@@ -21,6 +24,7 @@ import { AdresesComponent } from './adreses/adreses.component';
   ],
   providers: [
 //    { provide: BASE_PATH, useValue: "http:/localhost:8080/adres/api/v1" } 
+    {provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
