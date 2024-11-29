8275121807431b91b49af6e3db62d631debe32c9
bvpelt <bart.vanpelt@gmail.com>
2024-11-29 14:23:01 +0100

    PWA

--- adresbook/src/app/app.module.ts
+++ adresbook/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiModule } from './core/modules/openapi';
+import { environment } from '../environments/environment';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,6 +13,7 @@ import { BASE_PATH } from './core/modules/openapi';
 import { AdresesComponent } from './adreses/adreses.component';
 import { AdresComponent } from './adres/adres.component';
 import { AdresdetailComponent } from './adresdetail/adresdetail.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,13 @@ import { AdresdetailComponent } from './adresdetail/adresdetail.component';
     FormsModule,
     AppRoutingModule,
     ApiModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
 //    { provide: BASE_PATH, useValue: "http:/localhost:8080/adres/api/v1" }     
