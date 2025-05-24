6f3a0ffe7101bc0d2ad7fa5f726d8d18fef3cbad
Bart van Pelt <bart.vanpelt@gmail.com>
2025-05-24 19:07:08 +0200

    WIP

--- build.bash
+++ build.bash
@@ -9,3 +9,7 @@
 # Build and push image to dockerhub
 mvn -DactiveProfile=runtime clean package jib:build
 
+# curl -H "x-api-key:f0583805-03f6-4c7f-8e40-f83f55b7c077" http://localhost:8080/adres/api/v1/adresses
+
+# curl -H "x-api-key:f0583805-03f6-4c7f-8e40-f83f55b7c077" http://172.17.0.1:8080/adres/api/v1/adresses
+
